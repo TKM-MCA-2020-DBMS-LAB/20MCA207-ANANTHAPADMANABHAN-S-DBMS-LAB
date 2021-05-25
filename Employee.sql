e5b6c0708f2bac646b8b17b79b7a4debad602f6d
ANANTHAPADMANABHAN S <74697167+ananthapadmanabhans@users.noreply.github.com>
2021-05-25 10:35:19 +0530

    Delete Employee.sql

--- Employee.sql
+++
@@ -1,5 +0,0 @@
-create database DBMSLAB;
-use DBMSLAB;
-create table employee(emp_no int(20) NOT NULL PRIMARY KEY,emp_name varchar(50) NOT NULL,DOB date NOT NULL,address varchar(200) NOT NULL,DOJ date NOT NULL,mob_no int(20) NOT NULL,dept_no int(20) DEFAULT NULL,salary int(200) NOT NULL);
-DESC employee;
-create table department(dept_no int(20) NOT NULL,dept_name varchar(20) NOT NULL,location varchar(20) NOT NULL);
